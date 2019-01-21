b0af818a2b073fd9e6634359d679c0f168822872
Dipak84 <dipakbjdhav@gmail.com>
2019-01-21 11:39:55 +0530

    changes

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+	
+app.use(express.static(path.join(__dirname, '/dist')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// app.listen(process.env.PORT || 4201, () => {
+//   console.log('Connected to Port'); //Listening on port 8888
+// });
+
+app.set( 'port', ( process.env.PORT || 4201 ));
+
+// Start node server
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
